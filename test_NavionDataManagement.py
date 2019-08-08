1122d3d52e2e058e9195b5dc9e2ebef2ea35ae87
Jacob Ditslear <jake.ditslear@gmail.com>
2019-08-08 13:46:26 -0400

    Tested import NavionDataManagement

---
+++ test_NavionDataManagement.py
@@ -0,0 +1,9 @@
+#%%
+import sys
+sys.path = ['C:\\Users\\JDits001\\AppData\\Local\\Temp\\84e7d541-4776-4286-bcbd-c304637d05ce', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37\\python37.zip', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37\\DLLs', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37\\lib', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37', '', 'C:\\Users\\JDits001\\AppData\\Roaming\\Python\\Python37\\site-packages', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages', 'C:\\Users\\JDits001\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages\\IPython\\extensions', 'C:\\Users\\JDits001\\.ipython']
+sys.path.append('C:\\Users\\JDits001\\Documents\\Projects\\NavionDataManagement')
+
+from NavionDataManagement.NavionDataManagement import FileManagement
+FileManagement("Test.")
+
+#%%
